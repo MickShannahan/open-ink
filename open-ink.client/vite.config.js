cf93c09b3dece4cf4eca51d46fe8bd11ae88628d
mick shannahan <>
2023-01-29 17:20:36 -0700

    first deploy

--- open-ink.client/vite.config.js
+++ open-ink.client/vite.config.js
@@ -5,7 +5,7 @@ import { defineConfig } from 'vite'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'docs',
+    outDir: '../open-ink/client',
     sourcemap: false
   },
   server: {
