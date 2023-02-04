7e58f2f71548be6097c34d839b83365398089659
mick shannahan <beasticorn@gmail.com>
2023-02-04 13:59:14 -0700

    better ui?

---
+++ open-ink.client/src/utils/time.js
@@ -0,0 +1,6 @@
+export const seconds = 1000
+export const minutes = 60 * seconds
+export const hours = 60 * minutes
+export const days = 24 * hours
+export const months = 30 * days
+export const years = 12 * months
